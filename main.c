5b5f1c8beeb66936c0498d45c3c3ee43695560e1
Kevin_BEDIRIAN <k.bedir@outlook.fr>
2025-04-29 00:04:10 +0200

    Débug main

--- main.c
+++ main.c
@@ -15,22 +15,22 @@ int main(void)
     allegro_exit();
     return 0;
 }
-
+END_OF_MAIN();
 
 
 
 
 void initialisation_allegro(){
-    allegro_init(); // appel obligatoire (var.globales, recup. infos système ...)
+    allegro_init(); // Appel obligatoire (var. globales, recup. infos système ...)
     install_keyboard(); //pour utiliser le clavier
     install_mouse(); //pour utiliser la souris
     //pour choisir la profondeur de couleurs (8,16,24 ou 32 bits)
-    //ici: identique à celle du bureau
+    //ici : identique à celle du bureau
     set_color_depth(desktop_color_depth());
     //sélection du mode graphique
     // avec choix d'un driver+mode+résolution de l'écran
     if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0){
-        // message et sortie de prog. si échec
+        // Message et sortie de prog. Si échec
         allegro_message("prb gfx mode");
         allegro_exit();
         exit(EXIT_FAILURE);
