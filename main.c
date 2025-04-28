d719fc5e484e4bfe7866af347d4644cbc94d2825
sarrahgroupe9 <sarrah.essomba@edu.ece.fr>
2025-04-28 21:50:13 +0200

    3 eme commit

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <allegro.h>
+
+
+void initialisation_allegro();
+
+
+
 
 int main(void)
 {
+    initialisation_allegro();
     printf("Test github\n");
     printf("Test commit");
+    allegro_exit();
     return 0;
+}
+
+
+
+
+
+void initialisation_allegro(){
+    allegro_init(); // appel obligatoire (var.globales, recup. infos système ...)
+    install_keyboard(); //pour utiliser le clavier
+    install_mouse(); //pour utiliser la souris
+    //pour choisir la profondeur de couleurs (8,16,24 ou 32 bits)
+    //ici: identique à celle du bureau
+    set_color_depth(desktop_color_depth());
+    //sélection du mode graphique
+    // avec choix d'un driver+mode+résolution de l'écran
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0){
+        // message et sortie de prog. si échec
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
 }
