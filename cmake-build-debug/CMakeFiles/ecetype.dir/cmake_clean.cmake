e21a08c3e2201fe0e98c9f2e437b378a8ac0d6ce
Kevin_BEDIRIAN <k.bedir@outlook.fr>
2025-05-14 10:09:07 +0200

    sauvegarde2

---
+++ cmake-build-debug/CMakeFiles/ecetype.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecetype.dir/ennemis.c.obj"
+  "CMakeFiles/ecetype.dir/ennemis.c.obj.d"
+  "CMakeFiles/ecetype.dir/interface.c.obj"
+  "CMakeFiles/ecetype.dir/interface.c.obj.d"
+  "CMakeFiles/ecetype.dir/main.c.obj"
+  "CMakeFiles/ecetype.dir/main.c.obj.d"
+  "CMakeFiles/ecetype.dir/vaisseau.c.obj"
+  "CMakeFiles/ecetype.dir/vaisseau.c.obj.d"
+  "ecetype.exe"
+  "ecetype.exe.manifest"
+  "ecetype.pdb"
+  "libecetype.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ecetype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
